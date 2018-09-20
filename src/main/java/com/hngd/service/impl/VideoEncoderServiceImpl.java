0710d26e63356eedc13b571561ef7aa83a434674
tianp12345 <tianp12345@hotmail.com>
2018-09-20 14:33:34 +0800

    update

--- src/main/java/com/hngd/service/impl/VideoEncoderServiceImpl.java
+++ src/main/java/com/hngd/service/impl/VideoEncoderServiceImpl.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alibaba.druid.util.StringUtils;
